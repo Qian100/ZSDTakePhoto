19ea1c2b01c7549980665e027535bdc98f03e897
huxiaoqiao <huxiaoqiao@huxiaoqiaodeMac-mini.local>
2014-08-06 15:18:30 +0800

    拍照/从手机相簿选取照片

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZSDTakePhoto
+//
+//  Created by 胡晓桥 on 14-8-6.
+//  Copyright (c) 2014年 ZOSENDA GROUP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
